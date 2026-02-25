6a55a23c54026754733456edd055cdce55fcab59
AzabuMai <mgwk1120@outlook.com>
2026-02-26 00:57:32 +0800

    stack

--- DataStructure/Stack.java
+++ DataStructure/Stack.java
@@ -1,5 +1,31 @@
 package Algorithm.DataStructure;
 
-public class Stack {
+import java.util.ArrayList;
 
+public class Stack<T> {
+    private ArrayList<T> data;
+
+    public Stack() {
+        data = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public int size() {
+        return data.size();
+    }
+
+    public void add(T e) {
+        data.add(e);
+    }
+
+    public T peek() {
+        return data.getLast();
+    }
+
+    public T poll() {
+        return data.removeLast();
+    }
 }
